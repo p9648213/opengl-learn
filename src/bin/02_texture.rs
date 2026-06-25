b7463cb7f65adee6e2eae3524f95bf93c4ec9532
PL <phat.luong@codeforcevina.com>
2026-06-25 16:40:07 +0700

    texture

--- src/bin/02_texture.rs
+++ src/bin/02_texture.rs
@@ -1,14 +1,23 @@
+use image::GenericImageView;
 use opengl_learn::shader::Shader;
 use opengl_learn::{GlApp, run_app};
 use std::ffi::c_void;
 use std::ptr::null;
 
-struct ShaderLession {
-    vao: u32,
-    shader: Shader,
+struct Image {
+    width: u32,
+    height: u32,
+    nr_channel: u32,
+    data: Vec<u8>,
 }
 
-impl GlApp for ShaderLession {
+struct TextureLession {
+    vao: u32,
+    shader: Shader,
+    texture: u32,
+}
+
+impl GlApp for TextureLession {
     fn setup() -> Self {
         let vao = setup_scene();
         let mut shader = Shader::new();
@@ -22,7 +31,49 @@ impl GlApp for ShaderLession {
                 "/src/shader/02_texture/fragment.fs"
             ),
         );
-        Self { vao, shader }
+
+        let image = load_image(concat!(
+            env!("CARGO_MANIFEST_DIR"),
+            "/assets/texture/container.jpg"
+        ));
+
+        // texture
+        let mut texture: u32 = 0;
+
+        unsafe {
+            gl::GenTextures(1, &mut texture);
+            gl::BindTexture(gl::TEXTURE_2D, texture);
+
+            // set the texture wrapping/filtering options (on the currently bound texture object)
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::REPEAT as i32);
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::REPEAT as i32);
+            gl::TexParameteri(
+                gl::TEXTURE_2D,
+                gl::TEXTURE_MIN_FILTER,
+                gl::LINEAR_MIPMAP_LINEAR as i32,
+            );
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+
+            // generate the texture
+            gl::TexImage2D(
+                gl::TEXTURE_2D,
+                0,
+                gl::RGB as i32,
+                image.width as i32,
+                image.height as i32,
+                0,
+                gl::RGB,
+                gl::UNSIGNED_BYTE,
+                image.data.as_ptr() as *const c_void,
+            );
+            gl::GenerateMipmap(gl::TEXTURE_2D);
+        }
+
+        Self {
+            vao,
+            shader,
+            texture,
+        }
     }
 
     fn render(&mut self) {
@@ -30,32 +81,32 @@ impl GlApp for ShaderLession {
             gl::ClearColor(0.2, 0.3, 0.3, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
             self.shader.use_shader();
+            gl::BindTexture(gl::TEXTURE_2D, self.texture);
             gl::BindVertexArray(self.vao);
-            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+            gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, null());
         }
     }
 }
 
 fn setup_scene() -> u32 {
-    let vertices: [f32; 18] = [
-        // positions    // colors
-        0.5, -0.5, 0.0, 1.0, 0.0, 0.0, // bottom right
-        -0.5, -0.5, 0.0, 0.0, 1.0, 0.0, // bottom left
-        0.0, 0.5, 0.0, 0.0, 0.0, 1.0, // top
+    let vertices: [f32; 32] = [
+        // positions     // colors      // texture coords
+        0.5, 0.5, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, // top right
+        0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, // bottom right
+        -0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, // bottom left
+        -0.5, 0.5, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, // top left
     ];
 
-    let tex_coords: [f32; 6] = [
-        0.0, 0.0, // lower-left corner
-        1.0, 0.0, // lower-right corner
-        0.5, 1.0, // top-center corner
-    ];
+    let indices: [u32; 6] = [0, 1, 2, 2, 3, 0];
 
     let mut vao = 0;
     let mut vbo = 0;
+    let mut ebo = 0;
 
     unsafe {
         gl::GenVertexArrays(1, &mut vao);
         gl::GenBuffers(1, &mut vbo);
+        gl::GenBuffers(1, &mut ebo);
 
         gl::BindVertexArray(vao);
 
@@ -66,6 +117,13 @@ fn setup_scene() -> u32 {
             vertices.as_ptr() as *const c_void,
             gl::STATIC_DRAW,
         );
+        gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
+        gl::BufferData(
+            gl::ELEMENT_ARRAY_BUFFER,
+            (indices.len() * size_of::<u32>()) as isize,
+            indices.as_ptr() as *const c_void,
+            gl::STATIC_DRAW,
+        );
 
         // position attribute
         gl::VertexAttribPointer(
@@ -73,7 +131,7 @@ fn setup_scene() -> u32 {
             3,
             gl::FLOAT,
             gl::FALSE,
-            (6 * size_of::<f32>()) as i32,
+            (8 * size_of::<f32>()) as i32,
             null(),
         );
         gl::EnableVertexAttribArray(0);
@@ -84,19 +142,40 @@ fn setup_scene() -> u32 {
             3,
             gl::FLOAT,
             gl::FALSE,
-            (6 * size_of::<f32>()) as i32,
+            (8 * size_of::<f32>()) as i32,
             (3 * size_of::<f32>()) as *const c_void,
         );
         gl::EnableVertexAttribArray(1);
 
-        // texture
-        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::MIRRORED_REPEAT as i32);
-        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::MIRRORED_REPEAT as i32);
+        // texture attribut
+        gl::VertexAttribPointer(
+            2,
+            2,
+            gl::FLOAT,
+            gl::FALSE,
+            (8 * size_of::<f32>()) as i32,
+            (6 * size_of::<f32>()) as *const c_void,
+        );
+        gl::EnableVertexAttribArray(2);
 
         vao
     }
 }
 
+fn load_image(path: &str) -> Image {
+    let img = image::open(path).expect("Failed to load image");
+    let (width, height) = img.dimensions();
+    let rgb = img.flipv().to_rgb8();
+    let data = rgb.into_raw();
+
+    Image {
+        width,
+        height,
+        nr_channel: 3,
+        data,
+    }
+}
+
 fn main() {
-    run_app::<ShaderLession>("Shader");
+    run_app::<TextureLession>("Texture");
 }
